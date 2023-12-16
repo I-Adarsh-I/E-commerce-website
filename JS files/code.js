61a647aaf10b31b0dbf4cc41e834959413822784
@darsh_singh <adarsh@swatalliance.com>
2023-12-16 22:35:21 +0530

    Icon and logo has been set.

--- JS files/code.js
+++ JS files/code.js
@@ -5,7 +5,7 @@ class InsertEle extends HTMLElement {
     this.innerHTML = `    
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="index.html">Navbar</a>
+      <a class="navbar-brand" href="index.html"><img src = "../images/logos/logo.png" alt = "Shoper" width ="100%"/></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
       </button>  
